47c69855c890c0032b8c9b0554986e01d87b73d8
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-09 16:13:09 -0300

    Implementação para numeros inteiros.

---
+++ GenericRestrit/Program.cs
@@ -0,0 +1,23 @@
+﻿using Services.GenericRestrit;
+
+namespace GenericRestrit{
+    class Program{
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++){
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+            Console.WriteLine($"Max: {max}");
+        }
+    }
+}
